a5f4e2cd06724c81eb627f7dc87507fc955e6e64
Kevin Kelley <kkelley622@gmail.com>
2022-02-03 14:53:14 -0600

    commit tree 02172bb914264b419701c95e260533f09ca62f0e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.querySelector('#text')
+    paragraph.textContent = "This is really cool!"
+    console.log(paragraph)
+
+});
+
